7b0672c0f6332e9096b7756dda4cf42f8c156800
Zareth Alejandra Rodriguez Padilla <zareth.alejandra@gmail.com>
2025-09-02 18:54:27 +0200

    Delete Practica 2.py

--- Practica 2.py
+++
@@ -1,7 +0,0 @@
-print("\n\n")
-frase = input("Ingresa la oracion que quieras: ")
-print("Lista de palabras: ", frase.split())
-print("Palabras en mayus:")
-frase = frase.upper()
-for frase in frase.split():
-    print(frase)
